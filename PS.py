27dba82ae76cf88daafe902d9d371064f26a78a3
Abdu <abdussalam.elturki@toshiba-trel.com>
2018-07-26 15:22:00 +0100

    Made this single core, will bring back soon

--- PS.py
+++ PS.py
@@ -4,77 +4,88 @@ import socket as sck
 import numpy as np
 import Model_DNN as mdnn
 import Communication as com
-import threading
+# import mnist
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_float('learning_rate', -0.001,
+tf.app.flags.DEFINE_integer('learning_rate', -0.1,
                             """ Learning rate of the gradient descent.""")
+
+
+# ADACOMP - compute staleness
+def count_pred(history,key,loc):
+    if history == [] :
+        st = [0 for i in loc]
+    else:
+        st = np.add.reduce([[1 if i in h[key] else 0 for i in loc] for h in history], axis=0)
+    return st
+
 
 # Parameter Server routine
 def PS():
     with tf.Graph().as_default() as graph:
+      
         # Get input and labels for learning from D
         inputs, labels = tf.placeholder(tf.float32,shape=[None,FLAGS.image_size,FLAGS.image_size,FLAGS.image_depth]), tf.placeholder(tf.float32,shape=[None,FLAGS.nb_classes])
+        
         # Build a Graph that computes the logits predictions from the
         # inference model.
         logits = mdnn.CNN_model(inputs,graph)
+        
         # Build an initialization operation to run below.
         init = tf.global_variables_initializer()
         init_glob = tf.variables_initializer(tf.get_collection("W_global"))
+
         update_op = df.apply_sparse_update(graph,"W_global")
+
+      
         with tf.Session(graph=graph) as sess:
+      
             # Initialize the Deep Neural Network
             sess.run([init,init_glob])
 
             # Configure socket
             tcpsock = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
             tcpsock.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
+            
             tcpsock.bind(("",FLAGS.port))
 
-            history = []
             iteration = 0
-
             while iteration <FLAGS.iter_max+FLAGS.nb_workers-1:
-                while 1:
-                    #print "waiting to listen"
-                    tcpsock.listen(2)
-                    (wsocket, (ip, port)) = tcpsock.accept()
-                    #print wsocket
-                    threading.Thread(target=listenToWorker,
-                                     args=(wsocket,sess,graph,iteration,update_op)).run()
-                    #print "done listening"
-    print "PS is closed"
+                tcpsock.listen(1)
+                (wsocket, (ip, port)) = tcpsock.accept()
+                cmd,data = com.recv_msg(wsocket)
+                if cmd == "GET_W":
+                    #Encode parameter
+                    parameters = com.encode_variables(sess,"W_global",iteration,compression=1)
+                    com.send_msg(wsocket,parameters,"PARAM")
+                    wsocket.close()
+                elif cmd =="PUSH":
+                    old_iter,gradients = com.decode_variables(data)
+                    delay = iteration-old_iter
 
-def listenToWorker(wsocket,sess,graph,iteration,update_op):
-    cmd,data = com.recv_msg(wsocket)
-    if cmd == "GET_W":
-        #Encode parameter
-        parameters = com.encode_variables(sess,"W_global",iteration,compression=1)
-        com.send_msg(wsocket,parameters,"PARAM")
-        wsocket.close()
-    elif cmd =="PUSH":
-        old_iter,gradients = com.decode_variables(data)
-        new_gradients = {}
-        # for each trainable variable of the model
-        for k in gradients.keys():
-            #Multiple with learning rate
-            new_gradients[k] = np.asarray([FLAGS.learning_rate*gradients[k][i]
-                                           for i in range(len(gradients[k]))])
+                    # for each trainable variable of the model
+                    for k in gradients.keys():
+                        #Compute staleness for each parameter
+                        gradients[k] = [FLAGS.learning_rate*gradients[k][i] for i in range(len(gradients[k]))]
+                    
+                    # Update paramters
+                    feed_dict = {}
+                    for k in gradients.keys():
+                        feed_dict[k[:-5]+"_delta:0"]=gradients[k]
 
-        # Update paramters
-        feed_dict = {}
-        for k in gradients.keys():
-            feed_dict[k[:-5]+"_delta:0"]=new_gradients[k]
+                    sess.run(update_op,feed_dict)
+                    # Add update to history
 
-        sess.run(update_op,feed_dict)
-        # Add update to history
-        iteration+=1
-        cmd,data = com.recv_msg(wsocket)
-        if cmd == "GET_W":
-            #Encode parameter
-            parameters = com.encode_variables(sess,"W_global",iteration,compression=1)
-            com.send_msg(wsocket,parameters,"PARAM")
-            wsocket.close()
-        else:
-            wsocket.close()
+                    iteration+=1
+                    cmd,data = com.recv_msg(wsocket)
+                    if cmd == "GET_W":
+                        #Encode parameter
+                        parameters = com.encode_variables(sess,"W_global",iteration,compression=1)
+                        com.send_msg(wsocket,parameters,"PARAM")
+                        wsocket.close()
+                    else:
+                        wsocket.close()
+            
+            print "PS is closed"
+
